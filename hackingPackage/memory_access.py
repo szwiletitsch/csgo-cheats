36a589e328514d51160c0760ace67b1b1eb61f89
szwiletitsch <szwiletitsch@gmail.com>
2023-08-09 13:55:12 +0200

    fixed import

--- hackingPackage/memory_access.py
+++ hackingPackage/memory_access.py
@@ -1,9 +1,8 @@
-import win32api
 import win32gui
 import win32process
 import pymem
 
-from vector import Vec3
+from hackingPackage.vector import Vec3
 
 
 def get_handle_and_open_process_by_name(window_name: str):
@@ -21,7 +20,6 @@ def get_module(name, handle):
     list_of_modules = handle.list_modules()
     while list_of_modules is not None:
         tmp = next(list_of_modules)
-        print(tmp)
         if tmp[0].name == name:
             client_dll = tmp[1]
             return client_dll
@@ -36,11 +34,3 @@ def write_vec(handle, location, xyz):
     handle.write_float(location, float(xyz[0]))
     handle.write_float(location + 4, float(xyz[1]))
     handle.write_float(location + 8, float(xyz[2]))
-
-
-def is_pressed(key):
-    if win32api.GetKeyState(key) == -128 or win32api.GetKeyState(key) == -127:
-        return True
-    return False
-
-
